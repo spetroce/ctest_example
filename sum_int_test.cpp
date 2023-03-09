3da7f7c872d179a565be5ae0e984dc9d8a453445
Sam Petrocelli <sam.petrocelli@gmail.com>
2023-03-09 14:30:12 -0500

    update test

--- sum_int_test.cpp
+++ sum_int_test.cpp
@@ -1,7 +1,6 @@
 #include "my_math.h"
-#include <vector>
 
 int main() {
-  auto integers = {1, 2, 3, 4, 5};
+  std::vector<int> integers = {1, 2, 3, 4, 5};
   return mm::sum(integers) == 15 ? 0 : 1;
 }
